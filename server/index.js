21a4f5bbdf366050e5cc6146b15fafeb47fb1b7f
Luke Gammon <lukegammon@gmx.com>
2021-02-16 18:26:40 +0000

    update

--- server/index.js
+++ server/index.js
@@ -35,10 +35,39 @@ app.get("/todos", async(req,res) => {
 });
 
 //get a todo
-//https://youtu.be/ldYcgPKEZC8?t=1437
+
+app.get("/todos/:id",async(req,res) => {
+  try {
+    const {id} = req.params;
+    const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [id]);
+    res.json(todo.rows[0]);
+  } catch (e) {
+    console.error(e.message);
+  }
+});
 //update a todo
+app.put("/todos/:id", async(req,res) => {
+  try {
+    const {id} = req.params;
+    const {description} = req.body;
+    const updateTodo = await pool.query("UPDATE todo SET description = $1 WHERE todo_id = $2", [description, id]);
+    res.json("todo was updated");
+  } catch (e) {
+      console.error(e.message);
+  }
+});
 
 //delete a todo
+
+app.delete("/todos/:id", async(req,res) => {
+  try {
+    const {id} = req.params;
+    const deleteTodo = await pool.query("DELETE FROM todo WHERE todo_id = $1", [id]);
+    res.json("todo was deleted");
+  } catch (e) {
+      console.error(e.message);
+  }
+});
 
 const PORT = 5001;
 

3417c4a7c3937ae0a9aae6ecbcae0545b1b57f30
Luke Gammon <lukegammon@gmx.com>
2021-02-19 22:11:06 +0000

    update

--- client/src/components/ListTodos.js
+++ client/src/components/ListTodos.js
@@ -1,8 +1,21 @@
 import React, {Fragment, useEffect, useState} from "react";
-// Watch from Here... https://youtu.be/ldYcgPKEZC8?t=3552
+import EditTodo from "./EditTodo";
+
 const ListTodos = () => {
 
   const [todos, setTodos] = useState([]);
+
+  //delete todo function
+  const deleteTodo = async(id) => {
+      try {
+          const deleteTodo = await fetch(`http://localhost:5001/todos/${id}`, {
+            method: "DELETE"
+          });
+            setTodos(todos.filter(todo => todo.todo_id !== id));
+      } catch (err) {
+          console.error(err.message);
+      }
+  };
 
   const getTodos = async() => {
     try {
@@ -37,10 +50,10 @@ const ListTodos = () => {
     <td>john@example.com</td>
   </tr>*/}
   {todos.map(todo => (
-    <tr>
+    <tr key={todo.todo_id}>
       <td>{todo.description}</td>
-      <td>Edit</td>
-      <td>Delete</td>
+      <td><EditTodo todo={todo}/></td>
+      <td><button className="btn btn-danger" onClick={() => deleteTodo(todo.todo_id)}>Delete</button></td>
     </tr>
   ))}
   </tbody>
